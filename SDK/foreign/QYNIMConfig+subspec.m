7ae112299203eaaec0cd13f8c259b5a8cb534e66
zhongzhida <735132377@qq.com>
2022-07-27 17:27:31 +0800

    no message

--- SDK/foreign/QYNIMConfig+subspec.m
+++ SDK/foreign/QYNIMConfig+subspec.m
@@ -7,13 +7,23 @@
 //
 
 #import "QYNIMConfig+subspec.h"
-#import "NSObject+Utils.h"
+#import <objc/runtime.h>
 
 @implementation QYNIMConfig (subspec)
 + (void)load {
     [self swizzleMethods:QYNIMConfig.class originalSelector:@selector(isForeign) swizzledSelector:@selector(sw_isForeign)];
     [self swizzleMethods:QYNIMConfig.class originalSelector:@selector(nimLbsAddress) swizzledSelector:@selector(sw_nimLbsAddress)];
     [self swizzleMethods:QYNIMConfig.class originalSelector:@selector(nimLinkAddress) swizzledSelector:@selector(sw_nimLinkAddress)];
+}
++ (void)swizzleMethods:(Class)tagClass originalSelector:(SEL)origSel swizzledSelector:(SEL)swizSel {
+    Method origMethod = class_getInstanceMethod(tagClass, origSel);
+    Method swizMethod = class_getInstanceMethod(tagClass, swizSel);
+    BOOL didAddMethod = class_addMethod(tagClass, origSel, method_getImplementation(swizMethod), method_getTypeEncoding(swizMethod));
+    if (didAddMethod) {
+        class_replaceMethod(tagClass, swizSel, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    } else {
+        method_exchangeImplementations(origMethod, swizMethod);
+    }
 }
 - (BOOL)sw_isForeign {
     return YES;
